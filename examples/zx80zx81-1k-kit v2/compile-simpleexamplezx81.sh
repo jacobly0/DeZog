9b8b863f25392e3b3d1fe00c075bef9d2667c093
steve <warste@gmail.com>
2022-02-18 16:11:36 +0100

    sym creation

--- examples/zx80zx81-1k-kit v2/compile-simpleexamplezx81.sh
+++ examples/zx80zx81-1k-kit v2/compile-simpleexamplezx81.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 ./fasmg simpleexamplezx81.asm simpleexamplezx81.p > simpleexamplezx81.lst
-python3 makesld.py simpleexamplezx81.lst simpleexamplezx81.sld
+python3 makesld.py simpleexamplezx81.lst simpleexamplezx81.sld simpleexamplezx81.sym
