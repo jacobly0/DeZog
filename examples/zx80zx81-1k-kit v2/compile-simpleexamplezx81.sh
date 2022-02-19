7c2fc0464d935ee005cd8b1cd617931c7b72d169
steve <warste@gmail.com>
2022-02-19 23:27:43 +0100

    makesld

--- examples/zx80zx81-1k-kit v2/compile-simpleexamplezx81.sh
+++ examples/zx80zx81-1k-kit v2/compile-simpleexamplezx81.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 ./fasmg simpleexamplezx81.asm simpleexamplezx81.p > simpleexamplezx81.lst
 python3 makesld.py simpleexamplezx81.lst simpleexamplezx81.sld simpleexamplezx81.sym
+echo Done
+
